e75d0f7d1d22ea8a6a6f060e014de46729183427
dagota12 <84585431+dagota12@users.noreply.github.com>
2023-12-23 14:24:26 +0300

    Added solution for Distance Between Bus Stops

---
+++ solutions/distance-between-bus-stops.py
@@ -0,0 +1,6 @@
+class Solution:
+    def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
+        if start > destination:
+            start,destination=destination,start
+        return min(sum(distance[start:destination]),sum(distance) - sum(distance[start:destination]))
+        # 1 2 3 4
