68fad21b5708bba7c14d39b83adfdab054969661
dagota12 <84585431+dagota12@users.noreply.github.com>
2023-12-25 13:49:48 +0300

    Added solution for Keyboard Row

---
+++ solutions/keyboard-row.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findWords(self, words: List[str]) -> List[str]:
+        res =[]
+        rows ={ 0 : set("qwertyuiopQWERTYUIOP"), 1 : set("asdfghjklASDFGHJKL"), 2 : set("zxcvbnmZXCVBNM") }
+        for word in words:
+            s = set(word)
+            if s.issubset(rows[0]) or s.issubset(rows[1]) or s.issubset(rows[2]):
+                res.append(word)
+        return res
