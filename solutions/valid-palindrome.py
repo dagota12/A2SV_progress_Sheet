dc14496b18387d01bffc1993144df01756c04a7e
dagota12 <84585431+dagota12@users.noreply.github.com>
2024-01-04 14:47:42 +0300

    Added solution for Valid Palindrome

---
+++ solutions/valid-palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        res = ""
+        for l in s:
+            if l.isalnum():
+                res+=l
+        return res.lower() == res[::-1].lower()
+        
