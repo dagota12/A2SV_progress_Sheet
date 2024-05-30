c09f0d51483ec8a1aa5ba7f1239251fb7b1299fe
dagota12 <84585431+dagota12@users.noreply.github.com>
2024-05-30 03:10:29 +0300

    Created 'A - Ascend' by A2SV Hub

---
+++ A2SV G5 - Contest #18 30-May-2024/A - Ascend 162020.py
@@ -0,0 +1,23 @@
+# Problem: A - Ascend - https://codeforces.com/gym/526229/problem/A
+
+import sys
+from collections import defaultdict,Counter,deque
+input = lambda : sys.stdin.readline().strip()
+def print(*obj, sep=' ', end='\n'):
+    objs = [str(i) for i in obj]
+    sys.stdout.write(sep.join(objs) + end)
+def lis_int():
+    return [int(i) for i in input().split()]
+def lis_ch():
+    return [i for i in input().split()]
+n = int(input())
+nums=lis_int()
+mx = 1
+l = 1
+for i in range(1,n):
+    if nums[i-1] >= nums[i]:
+        l = 1
+    else:
+        l+=1
+    mx = max(mx,l)
+print(mx)
