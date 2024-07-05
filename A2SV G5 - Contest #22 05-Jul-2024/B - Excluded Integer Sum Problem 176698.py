ee8fec4f211f1e2691f6c2825e1c168937293fbb
dagota12 <84585431+dagota12@users.noreply.github.com>
2024-07-05 21:40:21 +0300

    Created 'B - Excluded Integer Sum Problem' by A2SV Hub

---
+++ A2SV G5 - Contest #22 05-Jul-2024/B - Excluded Integer Sum Problem 176698.py
@@ -0,0 +1,28 @@
+# Problem: B - Excluded Integer Sum Problem - https://codeforces.com/gym/531455/problem/B
+
+import sys
+from collections import defaultdict,Counter,deque
+from math import ceil
+input = lambda : sys.stdin.readline().strip()
+def print(*obj, sep=' ', end='\n'):
+    objs = [str(i) for i in obj]
+    sys.stdout.write(sep.join(objs) + end)
+def lis_int():
+    return [int(i) for i in input().split()]
+def lis_ch():
+    return [i for i in input().split()]
+t = int(input())
+for _ in range(t):
+    n, k, x = lis_int()
+    if x != 1:
+        print("YES")
+        print(n)
+        print(*([1] * n))
+    elif k == 1 or (k == 2 and n % 2):
+	    print("NO")
+
+    else:
+        print("YES")
+        print(n // 2)
+            
+        print(*([2] * (n // 2 - 1)) + [3 if n % 2 == 1 else 2])
