cdc94ddc61c6342ffce842c7121db6d3fd85115f
dagota12 <84585431+dagota12@users.noreply.github.com>
2024-07-18 23:48:31 +0300

    Created 'Z-Sort' by A2SV Hub

---
+++ 18-Jul-2024/Z-Sort 181702.py
@@ -0,0 +1,20 @@
+# Problem: Z-Sort - https://codeforces.com/problemset/problem/652/B
+
+n = int(input())
+arr = [int(i) for i in input().split()]
+ 
+arr.sort()
+res = []
+p1,p2 = 0,len(arr) - 1
+turn = True
+for i in range(n):
+    if turn:
+        turn = False
+        res.append(arr[p1])
+        p1+=1
+    else:
+        turn = True
+        res.append(arr[p2])
+        p2-=1
+ 
+print(*res)
