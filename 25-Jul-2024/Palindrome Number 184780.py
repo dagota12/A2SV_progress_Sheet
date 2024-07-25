018393b57836871c30cb498febf18049a3765496
dagota12 <84585431+dagota12@users.noreply.github.com>
2024-07-25 22:38:28 +0300

    Created 'Palindrome Number' by A2SV Hub

---
+++ 25-Jul-2024/Palindrome Number 184780.py
@@ -0,0 +1,7 @@
+# Problem: Palindrome Number - https://leetcode.com/problems/palindrome-number/
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        return str(x) == str(x)[::-1]
